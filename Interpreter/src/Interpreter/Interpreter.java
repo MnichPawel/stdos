c460ea0db110f17478375001476b3ae142642ea4
KorbikKamil <korbik.gm@gmail.com>
2019-12-10 00:40:01 +0100

    Improved for testing.
    Version ready for testing.

--- Interpreter/src/Interpreter/Interpreter.java
+++ Interpreter/src/Interpreter/Interpreter.java
@@ -15,13 +15,13 @@ import static VirtualMemory.VirtualMemory.get_value;
 //TODO getRegisters Method
 public class Interpreter {
     /*==============================================STRUCTURES========================================================*/
-    static Map<String, Integer> arguments = new HashMap<String, Integer>();
-    static Vector<String> Instruction = new Vector<String>();
+    private static Map<String, Integer> arguments = new HashMap<>();
+    private static Vector<String> Instruction = new Vector<>();
 
-    static PCB pcb;
-    static int address;
+    private static PCB pcb;
+    private static int address;
 
-    public void Interpreter() { // Constructor
+    public Interpreter() { // Constructor
         PCB pcb = MM_getRUNNING();
         int address = pcb.getPC();
         /*=============================================ADD KEYS AND VALUES=========================================== */
@@ -42,27 +42,30 @@ public class Interpreter {
             //...IN PROGRESS
         //PROCESS OPERATION
         arguments.put("CP", 2);  //CREATE PROCESS
-        arguments.put("DP", 1)   //DELETE PROCESS
+        arguments.put("DP", 1);  //DELETE PROCESS
     }
 
-     static void getORDER(){  //
-        int download = 0;
+     private static void getORDER(){  //
+        int download;
         char temparse;
-        String temp = "";
+        //String temp = "";
+        StringBuilder temp = new StringBuilder();
         for(int i=0; i<2; i++)
         {
             download = get_value(address);
             temparse = (char)download;
-            temp += temparse;
+            //temp+=temparse;
+            temp.append(temparse);
             address++;
         }
-        Instruction.add(temp);
+        //Instruction.add(temp);
+        Instruction.add(temp.toString());
     }
 
-    static void getARGUMENTS(int n){
-        int download = 0;
+    private static void getARGUMENTS(int n){
+        int download;
         char temparse;
-        String temp = "";
+        String temp;
         for(int i=0; i<n; i++)
         {
             temp = "";
@@ -90,27 +93,28 @@ public class Interpreter {
             Instruction.add(temp);
         }
     }
-    /*JEŚLI TO CZYTASZ, POMÓŻ MI WYMYŚLEĆ JAK ZAPROGRAMOWAĆ TO INNACZEJ, PONIEWAŻ WZIĘCIE POD UWAGĘ WSZYSTKICH OPCJI TO
+    /*JEŚLI TO CZYTASZ, POMÓŻ MI WYMYŚLiĆ JAK ZAPROGRAMOWAĆ TO INNACZEJ, PONIEWAŻ WZIĘCIE POD UWAGĘ WSZYSTKICH OPCJI TO
     * BĘDZIE MASAKRYCZNA ILOŚĆ KODU DO NAPISANIA*/
-    static void makeINSTRUCTION(){ //rejestr rejestr, rejestr adres, rejestr liczba
-        if(Instruction.get(0)=="AD"){
-            if (Instruction.get(1)=="AX"){
-                if (Instruction.get(2)=="["){
-
+    private static void makeINSTRUCTION(){ //rejestr rejestr, rejestr adres, rejestr liczba
+        if(Instruction.get(0).equals("AD")){
+            if (Instruction.get(1).equals("AX")){
+                if (Instruction.get(2).equals("[")){
+                    int ax = pcb.getAx(), value = get_value(Integer.parseInt(Instruction.get(3)));
+                    pcb.setAx(ax+value);
                 }
-                else if (Instruction.get(2)=="AX"){
-                    int ax = pcb.getAx(), aax = pcb.getAx();
-                    pcb.setAx(ax+aax);
+                else if (Instruction.get(2).equals("AX")){
+                    int ax = pcb.getAx();
+                    pcb.setAx(ax+ax);
                 }
-                else if (Instruction.get(2)=="BX"){
-                    int ax = pcb.getAx(), bx = pcb.getBx();
+                else if (Instruction.get(2).equals("BX")){
+                    int ax = pcb.getAx(), bx = pcb.getBx();                 //ELSEIF VERSION
                     pcb.setAx(ax+bx);
                 }
-                else if (Instruction.get(2)=="CX"){
+                else if (Instruction.get(2).equals("CX")){
                     int ax = pcb.getAx(), cx = pcb.getCx();
                     pcb.setAx(ax+cx);
                 }
-                else if (Instruction.get(2)=="DX"){
+                else if (Instruction.get(2).equals("DX")){
                     int ax = pcb.getAx(), dx = pcb.getDx();
                     pcb.setAx(ax+dx);
                 }
@@ -119,64 +123,90 @@ public class Interpreter {
                     pcb.setAx(ax+Integer.parseInt(Instruction.get(2)));
                 }
             }
-            else if (Instruction.get(1)=="BX"){
-                if (Instruction.get(2)=="["){
-
-                }
-                else if (Instruction.get(2)=="AX"){
-
-                }
-                else if (Instruction.get(2)=="BX"){
-
-                }
-                else if (Instruction.get(2)=="CX"){
-
-                }
-                else if (Instruction.get(2)=="DX"){
-
-                }
-                else{
-                    //zostaje opcja że to zwykła liczba
+            else if (Instruction.get(1).equals("BX")){
+                switch (Instruction.get(2)) {
+                    case "[": {
+                        int bx = pcb.getBx(), value = get_value(Integer.parseInt(Instruction.get(3)));
+                        pcb.setBx(bx + value);
+                        break;
+                    }
+                    case "AX": {
+                        int bx = pcb.getBx(), ax = pcb.getAx();
+                        pcb.setBx(bx + ax);
+                        break;
+                    }
+                    case "BX": {
+                        int bx = pcb.getBx();
+                        pcb.setBx(bx + bx);
+                        break;
+                    }                                      //SWITCH CASE VERSION
+                    case "CX": {
+                        int bx = pcb.getBx(), cx = pcb.getCx();
+                        pcb.setBx(bx + cx);
+                        break;
+                    }
+                    case "DX": {
+                        int bx = pcb.getBx(), dx = pcb.getDx();
+                        pcb.setBx(bx + dx);
+                        break;
+                    }
+                    default: {
+                        int bx = pcb.getBx();
+                        pcb.setBx(bx + Integer.parseInt(Instruction.get(2)));
+                        break;
+                    }
                 }
             }
-            else if (Instruction.get(1)=="CX"){
-                if (Instruction.get(2)=="["){
-
+            else if (Instruction.get(1).equals("CX")){
+                if (Instruction.get(2).equals("[")){
+                    int cx = pcb.getCx(), value = get_value(Integer.parseInt(Instruction.get(3)));
+                    pcb.setCx(cx+value);
                 }
-                else if (Instruction.get(2)=="AX"){
-
+                else if (Instruction.get(2).equals("AX")){
+                    int cx = pcb.getCx(), ax = pcb.getAx();
+                    pcb.setCx(cx + ax);
                 }
                 else if (Instruction.get(2)=="BX"){
-
+                    int cx = pcb.getCx(), bx = pcb.getBx();
+                    pcb.setCx(cx + bx);
                 }
                 else if (Instruction.get(2)=="CX"){
-
+                    int cx = pcb.getCx();
+                    pcb.setCx(cx + cx);
                 }
                 else if (Instruction.get(2)=="DX"){
-
+                    int cx = pcb.getCx(), dx = pcb.getDx();
+                    pcb.setCx(cx + dx);
                 }
                 else{
-                    //zostaje opcja że to zwykła liczba
+                    int cx = pcb.getCx();
+                    pcb.setCx(cx + Integer.parseInt(Instruction.get(2)));
                 }
             }
             else if (Instruction.get(1)=="DX"){
                 if (Instruction.get(2)=="["){
-
+                    int dx = pcb.getDx(), value = get_value(Integer.parseInt(Instruction.get(3)));
+                    pcb.setDx(dx+value);
                 }
                 else if (Instruction.get(2)=="AX"){
-
+                    int dx = pcb.getDx(), ax = pcb.getAx();
+                    pcb.setDx(dx + ax);
                 }
                 else if (Instruction.get(2)=="BX"){
-
+                    int dx = pcb.getDx(), bx = pcb.getBx();
+                    pcb.setDx(dx + bx);
                 }
                 else if (Instruction.get(2)=="CX"){
-
+                    int dx = pcb.getDx(), cx = pcb.getCx();
+                    pcb.setDx(dx + cx);
                 }
                 else if (Instruction.get(2)=="DX"){
-
+                    int dx = pcb.getDx();
+                    pcb.setDx(dx + dx);
                 }
                 else{
-                    //zostaje opcja że to zwykła liczba
+                    int dx = pcb.getDx();
+                    pcb.setCx(dx + Integer.parseInt(Instruction.get(2)));
                 }
             }
         }
@@ -184,7 +214,8 @@ public class Interpreter {
 
     public static void KK_Interpret(){
         getORDER();
-        getARGUMENTS(arguments.get(Instruction.firstElement());
+        getARGUMENTS(arguments.get(Instruction.firstElement()));
+        makeINSTRUCTION();
         pcb.setPC(address);
 
     }
