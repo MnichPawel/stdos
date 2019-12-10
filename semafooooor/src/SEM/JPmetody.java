f238ec6ea53dfd284ce50ac411b5c59008b4864a
StormageddonDarkLordOfAll <57874179+StormageddonDarkLordOfAll@users.noreply.github.com>
2019-12-10 09:59:00 +0100

    zpakowanko semaforka
    umiesciłem semaforek w packadźu i spackadżowan jest

---
+++ semafooooor/src/SEM/JPmetody.java
@@ -0,0 +1,54 @@
+package SEM;
+public class JPmetody {
+    private void wakeup(PCB p){
+        KM_setProcessState (p, ProcessState.READY);//zmiana stanu procesu na ready
+        MM_addReadyProcess(p);//dodanie procesu do listy kolejek priorytetowych
+    }
+    private void block(PCB p){
+        KM_setProcessState (p, ProcessState.WAITING);//zmiana stanu procesu na waiting
+        MM_unreadyProcess(p);//usuniecie procesu z listy kolejek priorytetowych
+    }
+   public void signal(semafor S){
+        S.wartosc+=1;
+        if(S.wartosc>0) {
+            PCB pom = S.kolejka.poll();
+            wakeup(pom);
+        }
+    }
+  public  void wait(semafor S){
+        S.wartosc-=1;
+        if(S.wartosc<=0) {
+            block(RUNNING);
+             S.kolejka.offer(RUNNING);
+        }
+    }
+    /*int JPwypisz(semafor S){ //wypisanie wartosci semafora
+        return S.wartosc;
+    }*/
+   public void JPwypiszOgolne(semafor S){ //wypisanie wartosci semafora zakladajaca ze semafory sa nie tylko w plikach
+       System.out.println(S.wartosc);
+    }
+  public  void JPwypisz(plik P){ //wypisanie wartosci semafora
+        System.out.println(P.sem.wartosc);
+    }
+   public void JPwypiszKolejke(plik P){ //wypisanie wartosci semafora
+        Deque<PCB> pom = P.sem.kolejka.clone(); //kopiowanie by zabezpieczyć się przed utratą zawartości oryginalnej kolejki
+        PCB pompcb;
+        for(int i=0; i<pom.size();i++){
+            pompcb=pom.pollFirst();
+            System.out.println(pompcb.pid,pompcb.pn);
+        }
+    }
+   public void JPwypiszKolejkeOgolne(semafor S){ //wypisanie wartosci semafora, wersja zakladajaca ze semafory sa nie tylko w plikach
+        Deque<PCB> pom = S.kolejka.clone(); //kopiowanie by zabezpieczyć się przed utratą zawartości oryginalnej kolejki
+        PCB pompcb;
+        for(int i=0; i<pom.size();i++){
+            pompcb=pom.pollFirst();
+            System.out.println(pompcb.pid,pompcb.pn);
+        }
+    }
+    public static int main(){
+
+        return 0;
+    }
+}
